b968c7bf70625a5f0f96018f901758dcad817666
VanillaMeow <leah.catgirlnyah@gmail.com>
2026-05-09 05:53:09 +0200

    main: Exit elegantly without traceback

--- src/moist_bot/__main__.py
+++ src/moist_bot/__main__.py
@@ -1,23 +1,24 @@
 from __future__ import annotations
 
 import asyncio
+from contextlib import suppress
 
 from .bot import MoistBot
 from .utils.setup_logging import setup_logging
 
 
-def main() -> None:
-    asyncio.run(_main())
+async def run_bot() -> None:
+    with setup_logging(), suppress(KeyboardInterrupt, asyncio.CancelledError):
+        async with MoistBot() as client:
+            await client.start()
 
 
 async def _main() -> None:
     await run_bot()
 
 
-async def run_bot() -> None:
-    with setup_logging():
-        async with MoistBot() as client:
-            await client.start()
+def main() -> None:
+    asyncio.run(_main())
 
 
 if __name__ == '__main__':
