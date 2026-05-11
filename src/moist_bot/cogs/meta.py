19355601a69bf9a9ba8e6f30b9554ca93f8673a9
VanillaMeow <leah.catgirlnyah@gmail.com>
2026-05-11 07:13:57 +0200

    meta: Remove some saypermission related commands

--- src/moist_bot/cogs/meta.py
+++ src/moist_bot/cogs/meta.py
@@ -6,18 +6,14 @@ import asyncio
 import inspect
 import itertools
 import unicodedata
-from collections import Counter
 from typing import TYPE_CHECKING, Any
 
 import discord
 from discord.ext import commands, menus
 
-from moist_bot.utils import formats, time
 from moist_bot.utils.paginator import RoboPages
 
 if TYPE_CHECKING:
-    import datetime
-
     from moist_bot.bot import MoistBot
     from moist_bot.utils.context import Context, GuildContext
 
@@ -368,253 +364,6 @@ class Meta(commands.Cog):
         """Quits the bot."""
         await self.bot.close()
 
-    @commands.command()
-    async def info(
-        self, ctx: Context, *, user: discord.Member | discord.User = commands.Author
-    ):
-        """Shows info about a user."""
-
-        e = discord.Embed()
-
-        if ctx.guild is not None and isinstance(user, discord.Member):
-            roles = [role.name.replace('@', '@\u200b') for role in user.roles]
-        else:
-            roles = []
-
-        e.set_author(name=str(user))
-
-        def format_date(dt: datetime.datetime | None) -> str:
-            if dt is None:
-                return 'N/A'
-            return f'{time.format_dt(dt, "F")} ({time.format_relative(dt)})'
-
-        e.add_field(name='ID', value=user.id, inline=False)
-
-        if ctx.guild is not None and isinstance(user, discord.Member):
-            e.add_field(name='Joined', value=format_date(user.joined_at), inline=False)
-
-        e.add_field(name='Created', value=format_date(user.created_at), inline=False)
-
-        badges_to_emoji = {
-            'partner': '<:partnernew:754032603081998336>',  # Discord Bots
-            'verified_bot_developer': '<:verifiedbotdev:853277205264859156>',  # Discord Bots
-            'hypesquad_balance': '<:balance:585763004574859273>',  # Discord Bots
-            'hypesquad_bravery': '<:bravery:585763004218343426>',  # Discord Bots
-            'hypesquad_brilliance': '<:brilliance:585763004495298575>',  # Discord Bots
-            'bug_hunter': '<:bughunter:585765206769139723>',  # Discord Bots
-            'hypesquad': '<:hypesquad_events:585765895939424258>',  # Discord Bots
-            'early_supporter': ' <:supporter:585763690868113455> ',  # Discord Bots
-            'bug_hunter_level_2': '<:goldbughunter:853274684337946648>',  # Discord Bots
-            'staff': '<:staff_badge:1087023029105725481>',  # R. Danny
-            'discord_certified_moderator': '<:certified_mod_badge:1087023030431129641>',  # R. Danny
-            'active_developer': '<:active_developer:1087023031332900894>',  # R. Danny
-        }
-
-        misc_flags_descriptions = {
-            'team_user': 'Application Team User',
-            'system': 'System User',
-            'spammer': 'Spammer',
-            'verified_bot': 'Verified Bot',
-            'bot_http_interactions': 'HTTP Interactions Bot',
-        }
-
-        set_flags = {flag for flag, value in user.public_flags if value}
-        subset_flags = set_flags & badges_to_emoji.keys()
-        badges = [badges_to_emoji[flag] for flag in subset_flags]
-
-        if ctx.guild is not None and ctx.guild.owner_id == user.id:
-            badges.append('<:owner:585789630800986114>')  # Discord Bots
-
-        if (
-            ctx.guild is not None
-            and isinstance(user, discord.Member)
-            and user.premium_since is not None
-        ):
-            e.add_field(
-                name='Boosted', value=format_date(user.premium_since), inline=False
-            )
-            badges.append('<:booster:1087022965775925288>')  # R. Danny
-
-        if badges:
-            e.description = ''.join(badges)
-
-        if (
-            ctx.guild is not None
-            and isinstance(user, discord.Member)
-            and user.voice is not None
-            and user.voice.channel is not None
-        ):
-            vc = user.voice.channel
-            other_people = len(vc.members) - 1
-            voice = (
-                f'{vc.name} with {other_people} others'
-                if other_people
-                else f'{vc.name} by themselves'
-            )
-            e.add_field(name='Voice', value=voice, inline=False)
-
-        if roles:
-            e.add_field(
-                name='Roles',
-                value=', '.join(roles) if len(roles) < 10 else f'{len(roles)} roles',
-                inline=False,
-            )
-
-        remaining_flags = (set_flags - subset_flags) & misc_flags_descriptions.keys()
-        if remaining_flags:
-            e.add_field(
-                name='Public Flags',
-                value='\n'.join(
-                    misc_flags_descriptions[flag] for flag in remaining_flags
-                ),
-                inline=False,
-            )
-
-        colour = user.colour
-        if colour.value:
-            e.colour = colour
-
-        e.set_thumbnail(url=user.display_avatar.url)
-
-        if ctx.guild is not None and isinstance(user, discord.User):
-            e.set_footer(text='This member is not in this server.')
-
-        await ctx.reply(embed=e)
-
-    @info.error
-    async def on_info_error(self, ctx: Context, error: commands.CommandError) -> None:
-        if isinstance(error, commands.BadUnionArgument):
-            error = error.errors[-1]
-            await ctx.reply(str(error))
-        else:
-            await ctx.reply(str(error))
-
-    @commands.command(aliases=['guildinfo'], usage='')
-    @commands.guild_only()
-    async def serverinfo(self, ctx: GuildContext, *, guild_id: int | None = None):
-        """Shows info about the current server."""
-
-        if guild_id is not None and await self.bot.is_owner(ctx.author):
-            guild = self.bot.get_guild(guild_id)
-            if guild is None:
-                return await ctx.reply('Invalid Guild ID given.')
-        else:
-            guild = ctx.guild
-
-        roles = [role.name.replace('@', '@\u200b') for role in guild.roles]
-
-        if not guild.chunked:
-            async with ctx.typing():
-                await guild.chunk(cache=True)
-
-        # figure out what channels are 'secret'
-        everyone = guild.default_role
-        everyone_perms = everyone.permissions.value
-        secret: Counter[type[discord.abc.GuildChannel]] = Counter()
-        totals: Counter[type[discord.abc.GuildChannel]] = Counter()
-        for channel in guild.channels:
-            allow, deny = channel.overwrites_for(everyone).pair()
-            perms = discord.Permissions((everyone_perms & ~deny.value) | allow.value)
-            channel_type = type(channel)
-            totals[channel_type] += 1
-            if not perms.read_messages or (
-                isinstance(channel, discord.VoiceChannel)
-                and (not perms.connect or not perms.speak)
-            ):
-                secret[channel_type] += 1
-
-        e = discord.Embed()
-        e.title = guild.name
-        e.description = f'**ID**: {guild.id}\n**Owner**: {guild.owner}'
-        if guild.icon:
-            e.set_thumbnail(url=guild.icon.url)
-
-        channel_info: list[str] = []
-        key_to_emoji: dict[type[discord.abc.GuildChannel], str] = {
-            discord.TextChannel: '<:text_channel:586339098172850187>',
-            discord.VoiceChannel: '<:voice_channel:586339098524909604>',
-        }
-        for key, total in totals.items():
-            secrets = secret[key]
-            try:
-                emoji = key_to_emoji[key]
-            except KeyError:
-                continue
-
-            if secrets:
-                channel_info.append(f'{emoji} {total} ({secrets} locked)')
-            else:
-                channel_info.append(f'{emoji} {total}')
-
-        info: list[str] = []
-        features = set(guild.features)
-        all_features = {
-            'PARTNERED': 'Partnered',
-            'VERIFIED': 'Verified',
-            'DISCOVERABLE': 'Server Discovery',
-            'COMMUNITY': 'Community Server',
-            'FEATURABLE': 'Featured',
-            'WELCOME_SCREEN_ENABLED': 'Welcome Screen',
-            'INVITE_SPLASH': 'Invite Splash',
-            'VIP_REGIONS': 'VIP Voice Servers',
-            'VANITY_URL': 'Vanity Invite',
-            'COMMERCE': 'Commerce',
-            'LURKABLE': 'Lurkable',
-            'NEWS': 'News Channels',
-            'ANIMATED_ICON': 'Animated Icon',
-            'BANNER': 'Banner',
-        }
-
-        for feature, label in all_features.items():
-            if feature in features:
-                info.append(f'{ctx.tick(True)}: {label}')  # noqa: FBT003
-
-        if info:
-            e.add_field(name='Features', value='\n'.join(info))
-
-        e.add_field(name='Channels', value='\n'.join(channel_info))
-
-        if guild.premium_tier != 0:
-            boosts = (
-                f'Level {guild.premium_tier}\n{guild.premium_subscription_count} boosts'
-            )
-            last_boost = max(
-                guild.members, key=lambda m: m.premium_since or guild.created_at
-            )
-            if last_boost.premium_since is not None:
-                boosts = f'{boosts}\nLast Boost: {last_boost} ({time.format_relative(last_boost.premium_since)})'
-            e.add_field(name='Boosts', value=boosts, inline=False)
-
-        bots = sum(m.bot for m in guild.members)
-        fmt = f'Total: {guild.member_count} ({formats.plural(bots):bot})'
-
-        e.add_field(name='Members', value=fmt, inline=False)
-        e.add_field(
-            name='Roles',
-            value=', '.join(roles) if len(roles) < 10 else f'{len(roles)} roles',
-        )
-
-        emoji_stats: Counter[str] = Counter()
-        for emoji in guild.emojis:
-            if emoji.animated:
-                emoji_stats['animated'] += 1
-                emoji_stats['animated_disabled'] += not emoji.available
-            else:
-                emoji_stats['regular'] += 1
-                emoji_stats['disabled'] += not emoji.available
-
-        fmt = (
-            f'Regular: {emoji_stats["regular"]}/{guild.emoji_limit}\n'
-            f'Animated: {emoji_stats["animated"]}/{guild.emoji_limit}\n'
-        )
-        if emoji_stats['disabled'] or emoji_stats['animated_disabled']:
-            fmt = f'{fmt}Disabled: {emoji_stats["disabled"]} regular, {emoji_stats["animated_disabled"]} animated\n'
-
-        fmt = f'{fmt}Total Emoji: {len(guild.emojis)}/{guild.emoji_limit * 2}'
-        e.add_field(name='Emoji', value=fmt, inline=False)
-        e.set_footer(text='Created').timestamp = guild.created_at
-        await ctx.reply(embed=e)
-
     @staticmethod
     async def say_permissions(
         ctx: Context,
@@ -637,42 +386,6 @@ class Meta(commands.Cog):
         e.add_field(name='Allowed', value='\n'.join(allowed))
         e.add_field(name='Denied', value='\n'.join(denied))
         await ctx.reply(embed=e)
-
-    @commands.command()
-    @commands.guild_only()
-    async def permissions(
-        self,
-        ctx: GuildContext,
-        member: discord.Member = commands.Author,
-        channel: discord.abc.GuildChannel | discord.Thread = commands.CurrentChannel,
-    ):
-        """Shows a member's permissions in a specific channel.
-
-        If no channel is given then it uses the current one.
-
-        You cannot use this in private messages. If no member is given then
-        the info returned will be yours.
-        """
-        await self.say_permissions(ctx, member, channel)
-
-    @commands.command()
-    @commands.guild_only()
-    async def botpermissions(
-        self,
-        ctx: GuildContext,
-        *,
-        channel: discord.abc.GuildChannel | discord.Thread | None = None,
-    ):
-        """Shows the bot's permissions in a specific channel.
-
-        If no channel is given then it uses the current one.
-
-        This is a good way of checking if the bot has the permissions needed
-        to execute the commands it wants to execute.
-        """
-        channel = channel or ctx.channel
-        member = ctx.guild.me
-        await self.say_permissions(ctx, member, channel)
 
     @commands.command()
     @commands.is_owner()
@@ -700,6 +413,7 @@ class Meta(commands.Cog):
         await self.say_permissions(ctx, member, channel)
 
     @commands.command(hidden=True)
+    @commands.cooldown(1, 30.0, type=commands.BucketType.user)
     async def cud(self, ctx: Context):
         """pls no spam"""
 
