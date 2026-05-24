fd9141bb44b10a05a3f2aeabb554d749d2d4b5bf
VanillaMeow <leah.catgirlnyah@gmail.com>
2026-05-24 23:55:46 +0200

    fleabot: Change bot prefixes

--- src/moist_bot/bot_fleabot.py
+++ src/moist_bot/bot_fleabot.py
@@ -2,11 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Unpack
 
+from discord.ext import commands
+
 from moist_bot.bot import MoistBot
 from moist_bot.settings import settings
 
 if TYPE_CHECKING:
     from typing import Final
+
+    from discord import Message
 
     from moist_bot.bot import BotOptions
 
@@ -21,10 +25,16 @@ FLEABOT_EXTENSIONS: Final = (
     'purge',
     'jsk',
 )
+BOT_PREFIXES = ('fb ', 'Fb ')
+
+
+def _get_prefix(bot: commands.Bot, message: Message) -> list[str]:
+    return commands.when_mentioned_or(*BOT_PREFIXES)(bot, message)
 
 
 class FleaBot(MoistBot):
     def __init__(self, **kwargs: Unpack[BotOptions]) -> None:
+        kwargs.setdefault('command_prefix', _get_prefix)
         super().__init__(startup_extensions=FLEABOT_EXTENSIONS, **kwargs)
 
     async def start(
