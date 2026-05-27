1259b5fc4d1dce43e334ce5ec572e8dd0cb9fbb7
VanillaMeow <leah.catgirlnyah@gmail.com>
2026-05-27 21:55:30 +0200

    context: Fix `Context.tick` unable to display emojis

--- src/moist_bot/utils/context.py
+++ src/moist_bot/utils/context.py
@@ -122,9 +122,9 @@ class Context(commands.Context[MoistBot]):
     @staticmethod
     def tick(opt: bool | None, label: str | None = None) -> str:
         lookup = {
-            True: '<:greenTick:330090705336664065>',
-            False: '<:redTick:330090723011592193>',
-            None: '<:greyTick:563231201280917524>',
+            True: '<:greenTick:1509282775692218390>',
+            False: '<:redTick:1509282740535689267>',
+            None: '<:greyTick:1509282598114037950>',
         }
         emoji = lookup.get(opt, '<:redTick:330090723011592193>')
         if label is not None:
