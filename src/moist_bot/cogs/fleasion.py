eeea85b201bca203f5e8129d6133c8201311632f
VanillaMeow <leah.catgirlnyah@gmail.com>
2026-09-11 23:50:19 +0200

    fleasion: Add experimental help feature

--- src/moist_bot/cogs/fleasion.py
+++ src/moist_bot/cogs/fleasion.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 import discord
 from discord.ext import commands
@@ -10,13 +10,24 @@ if TYPE_CHECKING:
     from moist_bot.bot import MoistBot
     from moist_bot.utils.context import Context
 
+    class GuildMessage(discord.Message):
+        """A message in a guild."""
+
+        guild: discord.Guild
+        author: discord.Member  # type: ignore[reportIncompatibleVariableOverride]
+
 
 log = logging.getLogger('discord.' + __name__)
 
 FLEASION_GUILD_ID = 1309760132770693181
-FLEASION_CHANNEL_IDS = frozenset(
+FLEASION_CLEANUP_CHANNEL_IDS = frozenset(
     (
         1309904275932975214,  # moderation-logs
+    )
+)
+FLEASION_HELP_CHANNEL_IDS = frozenset(
+    (
+        1495010741940654182,  # general
     )
 )
 
@@ -25,6 +36,11 @@ class Fleasion(commands.Cog):
     def __init__(self, bot: MoistBot):
         self.bot: MoistBot = bot
 
+        # Temporary until I figure out if this is viable
+        self.testing_channel = cast(
+            'discord.TextChannel', bot.get_channel(1548068976104579142)
+        )
+
     @property
     def display_emoji(self) -> discord.PartialEmoji:
         return discord.PartialEmoji(name='\N{CRICKET}')
@@ -32,10 +48,10 @@ class Fleasion(commands.Cog):
     def cog_check(self, ctx: Context) -> bool:  # type: ignore[]
         return bool(ctx.guild) and ctx.guild.id == FLEASION_GUILD_ID
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message) -> None:
+    @commands.Cog.listener(name='on_message')
+    async def on_cleanup_message(self, message: discord.Message):  # pyright: ignore[reportRedeclaration]
         bot_user = self.bot.user or 'Fleabot'
-        if message.channel.id not in FLEASION_CHANNEL_IDS:
+        if message.channel.id not in FLEASION_CLEANUP_CHANNEL_IDS:
             return
 
         if any(
@@ -43,6 +59,27 @@ class Fleasion(commands.Cog):
             for embed in message.embeds
         ):
             await message.delete()
+
+    @commands.Cog.listener(name='on_message')
+    async def on_help_message(self, message: discord.Message):
+        if message.channel.id not in FLEASION_HELP_CHANNEL_IDS:
+            return
+        message = cast('GuildMessage', message)
+
+        # Main criteria
+        if message.content not in {'help', 'how to'}:
+            return
+
+        # We want to catch only new members
+        for role in message.author.roles:
+            if '[' in role.name:  # Level role (e.g. "Meow [L1]")
+                break
+        else:
+            return
+
+        # TODO(leah): Figure out if this is viable
+        # For now just forward to the testing channel
+        await message.forward(self.testing_channel)
 
 
 async def setup(bot: MoistBot) -> None:
